eefec22777c59bf0b395df520ec5fe548df72f69
황규영 <kyuy9214@gmail.com>
2025-08-05 15:19:59 +0900

    feat: 유니태스크, 코루틴을 통해 일정 시간 대기 후 게임 오브젝트를 생성하는 클래스 구현

---
+++ Assets/Scripts/UniTaskWaitTime.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+
+public class UniTaskWaitTime : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject box;
+
+    [SerializeField]
+    private Vector3 coroutinePosition;
+
+    [SerializeField]
+    private Vector3 uniTaskPosition;
+
+    void Start()
+    {
+        // 코루틴을 통해 코루틴 메소드 실행
+        StartCoroutine(GenerateBoxCoroutine());
+
+        // 유니태스크 메소드 실행
+        // Forget()을 통해 유니태스크에서 발생하는 예외를 무시한다.
+        GenerateBoxUniTask().Forget();
+    }
+
+    //IEnumerator를 통해 코루틴을 사용하는 메소드
+    private IEnumerator GenerateBoxCoroutine()
+    {
+        yield return new WaitForSeconds(5.0f);
+        Instantiate(box, coroutinePosition, Quaternion.identity);
+    }
+
+    private async UniTaskVoid GenerateBoxUniTask()
+    {
+        // async void 사용시 메소드에서 예외가 발생하면 crash가 발생하므로 UniTaskVoid를 사용해야 한다.
+        // UniTask.Delay(int milliseconds)는 Task.Delay(int milliseconds)에 대응된다.
+        await UniTask.Delay(5000);
+        Instantiate(box, uniTaskPosition, Quaternion.identity);
+    }
+
+}
