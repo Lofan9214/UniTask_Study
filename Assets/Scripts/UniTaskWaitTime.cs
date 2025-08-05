b7c2c20bd04c40f23d441dcd619ed294a95eb78d
황규영 <kyuy9214@gmail.com>
2025-08-05 15:55:47 +0900

    style: 메소드명 변경

--- Assets/Scripts/UniTaskWaitTime.cs
+++ Assets/Scripts/UniTaskWaitTime.cs
@@ -17,26 +17,25 @@ public class UniTaskWaitTime : MonoBehaviour
     void Start()
     {
         // 코루틴을 통해 코루틴 메소드 실행
-        StartCoroutine(GenerateBoxCoroutine());
+        StartCoroutine(CoGenerateBox());
 
         // 유니태스크 메소드 실행
         // Forget()을 통해 유니태스크에서 발생하는 예외를 무시한다.
-        GenerateBoxUniTask().Forget();
+        TaskGenerateBox().Forget();
     }
 
     //IEnumerator를 통해 코루틴을 사용하는 메소드
-    private IEnumerator GenerateBoxCoroutine()
+    private IEnumerator CoGenerateBox()
     {
         yield return new WaitForSeconds(5.0f);
         Instantiate(box, coroutinePosition, Quaternion.identity);
     }
 
-    private async UniTaskVoid GenerateBoxUniTask()
+    private async UniTaskVoid TaskGenerateBox()
     {
         // async void 사용시 메소드에서 예외가 발생하면 crash가 발생하므로 UniTaskVoid를 사용해야 한다.
-        // UniTask.Delay(int milliseconds)는 Task.Delay(int milliseconds)에 대응된다.
+        // UniTask.Delay(int milliseconds)는 코루틴의 WaitForSeconds, Task.Delay(int milliseconds)에 대응된다.
         await UniTask.Delay(5000);
         Instantiate(box, uniTaskPosition, Quaternion.identity);
     }
-
 }
